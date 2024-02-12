97969eea9f385dccedfe2a3dc92c3c1166e4b3ec
md0011 <mayurd0303@gmail.com>
2024-02-12 15:35:12 +0530

    Objects

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 console.log("Hello World");
-
+// ------------------------------------------------------------------------
 let name = "Mayur"; // String Literal
 console.log(name);
-
+// ------------------------------------------------------------------------
 // Cannot be a reserved keyword
 // Should be meaningful
 // Cannot start with a number (1name)
 // Cannot contain a space or hyphen (-)
 // Are case-sensitive
-
+// ------------------------------------------------------------------------
 const interestRate = 0.3;
 // interestRate = 1;   --> Can't update a value of constant
 console.log(interestRate);
-
+// ------------------------------------------------------------------------
 let age = 30; // Number Literal
 let isApproved = false; // Boolean Literal
 let firstName = undefined;
 let selectedColor = null;
+// ------------------------------------------------------------------------
+let employee = {
+  id: 1,
+  employeeName: "Mosh",
+};
+// Dot Notation
+employee.employeeName = "John";
+// Bracket Notation
+employee["id"] = 2;
+console.log(employee.employeeName);
+// ------------------------------------------------------------------------
