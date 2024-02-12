1586646e4d191d364f82cc573968d1aa4104d19f
md0011 <mayurd0303@gmail.com>
2024-02-12 14:56:45 +0530

    Variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("Hello World");
+
+let name = "Mayur";
+console.log(name);
+
+// Cannot be a reserved keyword
+// Should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+let firstName = "Mayur";
+let lastName = "Dev";
